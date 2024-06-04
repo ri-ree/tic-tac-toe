4a3c4d1589141b14f392c578812dd07a3bb69302
ri-ree <adrie270801@gmail.com>
2024-06-04 20:08:46 +0200

    Add createGameboard function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
 const myGameboard = { 
-    gameboard: []
+    gameboard: [],
+    createGameboard: function () {
+        const columns = 6;
+        const rows = 6;
+            for (i = 0; i < columns; i++) {
+                gameboard[i] = [];
+                for (j = 0; j < rows; j++) {
+                    gameboard.push(createCell());
+                };
+            };
+    }
 };
